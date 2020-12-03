17d3e03abae6b2d5c78b823f2c4ecb581fa445fd
ZongYunhui <a18297529873@gmail.com>
2020-12-03 20:03:15 +0800

    smartPointer

--- grammer/SmartPtr.cpp
+++ grammer/SmartPtr.cpp
@@ -6,10 +6,10 @@ public:
 	int mId;
 	Task(int id){
 		this->mId=id;
-		cout<<"Constructor process"<<endl;
+		cout<<"Constructor process: "<<mId<<endl;
 	}
 	~Task(){
-		cout<<"Destructor process"<<endl;
+		cout<<"Destructor process: "<<mId<<endl;
 	}
 };
 void uniqueptrTest(){
@@ -18,8 +18,9 @@ void uniqueptrTest(){
 	staskptr=make_unique<Task>(32);//使用make_unique创建unique_ptr对象
 	Task *mTask=staskptr.get();//可通过.get()获得管理对象的指针。
 	cout<<mTask->mId<<endl;
-	staskptr.reset();//释放关联的原始指针，将unique_ptr置为空
+	staskptr.reset(new Task(11));//释放关联的原始指针，将unique_ptr置为空
 	cout<<mTask->mId<<endl;//打印0值，该块内存区域已经备置为空。
+	cout<<"reset test:"<<staskptr->mId<<endl;
 	mTask->mId=1;//依然可以访问
 	cout<<mTask->mId<<endl;
 	if(staskptr==nullptr){
@@ -38,8 +39,74 @@ void uniqueptrTest(){
 		cout<<mTask->mId<<endl;
 	}
 }
-int main(){
-	uniqueptrTest();
 
+void sharedptrTest(){
+	//构造：
+	shared_ptr<Task>p1(new Task(24));
+	cout<<"构造："<<p1->mId<<endl;
+	auto q=p1;//q(p1);
+	auto r=new Task(26);
+	q.reset(r);//可以将Task *强转为shared_ptr<Task>
+	//r=make_shared<Task>(27);不可以将shared_ptr<Task>强转为Task*
+	cout<<"countnum:"<<p1.use_count()<<endl;
+	shared_ptr<Task>p2(new Task(34));
+	//p2.reset(q.get());该行会导致重复释放内存报错
+	p2.swap(p1);
+	cout<<"p2 is"<<p2->mId<<"p1 is"<<p1->mId<<endl;
+	cout<<"析构test:"<<"  "<<q.use_count()<<endl;
+	auto p3=p2;
+	cout<<p2.use_count()<<endl;
+	p2.unique();
+	shared_ptr<Task> sizeShare;
+	cout<<p2.use_count()<<endl;
+	cout<<"size of shared_ptr:"<<sizeof(p2)<<"   "<<sizeof(sizeShare)<<endl;
+	unique_ptr<Task> sizeUnique;
+	cout<<"size of unique_ptr"<<sizeof(sizeUnique)<<endl;
+}
+
+void make_pointer(){
+	shared_ptr<Task> sp,tp;
+	tp=make_shared<Task>(3);	
+	sp.reset(new Task(2));
+	unique_ptr<Task> up,qp;
+	qp=make_unique<Task>(4);
+	//up.reset(qp);
+}
+//https://blog.csdn.net/hp_cpp/article/details/103452196 指定删除器
+template<typename T>
+shared_ptr<T> make_shared_array(size_t size)
+{
+	return shared_ptr<T>( new T[size],default_delete <T[]>());	
+}
+
+void deleter(Task *x){
+	cout<<"delete from deleter function"<<endl;
+	delete x;
+}
+
+class Deleter{
+	public:
+	void operator()(Task *x){
+		cout<<"delete from delete class"<<endl;
+		delete x;
+	}
+};
+
+
+void specifiedDeleter(){
+	//shared_ptr<Task>p1(new Task(34),deleter);//使用普通函数
+	//shared_ptr<Task>p2(new Task(35),Deleter());//使用仿函数
+	shared_ptr<Task>p3(new Task(36),[](Task *x){
+				cout<<"delete from lambda "<<endl;
+				delete x;
+				});
+
+}
+
+int main(){
+	//uniqueptrTest();
+	//sharedptrTest();
+	//make_pointer();
+	specifiedDeleter();
 	return 0;
 }
