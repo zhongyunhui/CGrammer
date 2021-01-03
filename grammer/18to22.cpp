1cb8f4ba611b2d126f914876f69f63643082f08e
zhongyunhui <18297529873@163.com>
2021-01-03 23:52:19 +0800

    add smart ptr

--- grammer/18to22.cpp
+++ grammer/18to22.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstdarg>
 #include<memory>
 using namespace std;
 
@@ -7,8 +8,50 @@ class Investment{
 		int x,y;
 	public:
 		Investment(int a):x(a){};
+		virtual int getX(){
+			return x;
+		}
+		virtual ~Investment(){
+			cout<<"delete by Investment"<<endl;
+		}
 };
 
-template<typename... Ts>
+class Stock:public Investment{
+	public:
+		Stock(int a):Investment(a){
+			cout<<"Construct stock"<<endl;
+		}
+		virtual int getX(){
+			Investment::getX();
+		}       
+};
+//lambda
+auto delInvmt=[](Investment* pInvest){
+	printf("delete by delInvmt\n");
+	delete pInvest;
+};
+
+
+template<typename Ts>
 unique_ptr<Investment>
-makeInvestment(Ts&&... params);
+makeInvestment(Ts params){
+	return unique_ptr<Investment>(move(params));
+}
+
+template<typename Ts,typename Td>
+unique_ptr<Investment,decltype(delInvmt)>
+makeInvestment(Ts params,Td dele){
+	return unique_ptr<Investment,decltype(delInvmt)>(params,dele);
+}
+
+void UniqueTest(){
+	auto pInvestment=makeInvestment(new Stock(3),delInvmt);
+	cout<<pInvestment->getX()<<endl;
+}
+
+int main(){
+	UniqueTest();
+	
+	
+	return 0;
+} 
