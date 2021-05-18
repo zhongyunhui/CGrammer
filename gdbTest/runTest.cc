01964e81156ec0ae838aa1d23ffccdb12d836ae1
ZongYunhui <a18297529873@gmail.com>
2021-05-18 23:32:59 +0800

    gdb thread

--- gdbTest/runTest.cc
+++ gdbTest/runTest.cc
@@ -1,12 +1,28 @@
 #include <iostream>
 #include<unistd.h>
+#include <thread>
+#include<atomic>
 using namespace std;
 int main(){
-	int i=100;
+	atomic_int i(300);
+	thread * t1=new thread([&](){
+		while(i>0){
+			i--;
+			sleep(1);
+			cout<<"threadID:"<<this_thread::get_id()<<" i:"<<i<<endl;
+			}
+		});
+	thread * t2=new thread([&](){
+		while(i>0){
+			i--;
+			sleep(1);
+			cout<<"threadID:"<<this_thread::get_id()<<" i:"<<i<<endl;
+			}
+		});
 	while(i>0){
 		i--;
 		sleep(1);
-		cout<<"Hello: "<<i<<endl;
+		cout<<"threadID:"<<this_thread::get_id()<<" i "<<i<<endl;
 	}
 	return 0;
 }
